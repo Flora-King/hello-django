78b486edaa38953089648bf5d607c77388bde8c8
Flora-King <giflona@yahoo.co.uk>
2023-08-31 19:25:20 +0000

    add changes to procfile

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-=6m#cobobux=to!27xxq8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOTSNAME')]
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
